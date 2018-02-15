bafe3f90c20a6088be607bf1b603617e9437a37d
AyyKamp <arda513@hotmail.de>
2018-02-15 19:54:14 +0100

    [-]deleted docs
    moved em to AyyKamp/TVSeriesDocs

--- firebase/firestore.js
+++ firebase/firestore.js
@@ -1,23 +1,11 @@
-const admin = require("./admin.js");
-const db = admin.firestore();
-
-db.collection("users").doc('fedor').set(***REMOVED***
-    token:"xddddddd",
-    id:34,
-    xd:"lmaoooooo"
-***REMOVED***).then(() =>***REMOVED***
-    console.log("haha lmao")
-***REMOVED***).catch(error=>***REMOVED***
-    console.log("Konrad wtf: " + error)
-***REMOVED***)
-
 var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
 var path = require('path');
-var admin = require(path.join(__dirname + "/../admin.js"));
 
-var html;
+var admin = require(path.join(__dirname + "/admin.js"));
+const db = admin.firestore();
+
 
 var middleware = function (req, res, next) ***REMOVED***
     res.header("Access-Control-Allow-Origin", "*");
@@ -28,6 +16,7 @@ var middleware = function (req, res, next) ***REMOVED***
 router.use(bodyParser.urlencoded(***REMOVED*** extended: true ***REMOVED***), middleware);
 
 router.post('/', function (req, res) ***REMOVED***
+    console.log(req.body)
     var editedBody = req.body;
     if (Object.values(req.body) == "") ***REMOVED***
         var editedBody = Object.keys(req.body)
@@ -37,15 +26,26 @@ router.post('/', function (req, res) ***REMOVED***
     var registrationToken = editedBody.token;
 
     var data = ***REMOVED***
-        id = editedBody.id,
-        token = editedBody.token,
+        "id": editedBody.id,
+        "token": editedBody.token
+    ***REMOVED***;
 
+    console.log(Object.keys(req.body))
+    var data3 = "***REMOVED***\"wl\":" + editedBody.watchlist + "***REMOVED***";
+    console.log(data3)
+    var data2 = ***REMOVED***
+        "watchlist": JSON.parse(data3).wl
     ***REMOVED***;
 
     db.collection("users").doc('fedor').set(data).then(() =>***REMOVED***
-        console.log(data.id + "added. Token: " + data.token)
+        console.log(data.id + " added. Token: " + data.token)
+        db.collection("users").doc('fedor').collection("watchlists").doc("watchlist:" + data.token).set(data2).then(() =>***REMOVED***
+            console.log("awwyiss");
+        ***REMOVED***).catch(err=>***REMOVED***
+            console.log("kaonrad wtf: " + err);
+        ***REMOVED***);
     ***REMOVED***).catch(error=>***REMOVED***
-        console.log("Konrad wtf: " + error)
+        console.log("Konrad wtf: " + error);
     ***REMOVED***)
     
 
