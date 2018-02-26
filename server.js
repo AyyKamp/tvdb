4c8cae180ea2cece2afb4bfe97aea849880f1e1e
AyyKamp <arda513@hotmail.de>
2018-02-26 22:38:11 +0100

    [*]dotenv

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const request = require("request");
 const ***REMOVED*** execSync ***REMOVED*** = require("child_process");
 var app = require("./app");
@@ -7,8 +8,8 @@ var shell = require('shelljs');
 var server = app.listen(port, function() ***REMOVED***
     console.log(`its working btw ($***REMOVED***port***REMOVED***) `);
 ***REMOVED***);
-let token = shell.exec('heroku auth:token').stdout;
-console.log(token)
+let token = shell.exec('heroku auth:token').stdout || process.env.HEROKU_KEY;
+console.log(process.env.HEROKU_KEY)
 var headers = ***REMOVED***
     Accept: "application/vnd.heroku+json; version=3",
     Authorization: "Bearer " + token.replace(/(\r\n|\n|\r)/gm, "")
