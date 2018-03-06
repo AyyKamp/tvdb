631cea97d82cafba2727b6c4dacf346c414f492f
AyyKamp <32194363+AyyKamp@users.noreply.github.com>
2018-03-06 14:08:31 +0100

    Update getWatchlist.js

--- POST/firebase/watchlist/getWatchlist.js
+++ POST/firebase/watchlist/getWatchlist.js
@@ -16,7 +16,7 @@ router.post('/', function (req, res) ***REMOVED***
                 for(let i of doc.data().watchlist) ***REMOVED***
                     request(***REMOVED***url:`http://tvdb-rest.herokuapp.com/getSeriesById?series_id=$***REMOVED***i***REMOVED***`***REMOVED***, (err, res, body) => ***REMOVED***
                         if (!err && res.statusCode == 200) ***REMOVED***
-                            console.log(body.seriesName)
+                            console.log(body)
                             arr[arr.length] = body
                         ***REMOVED***
                     ***REMOVED***)
