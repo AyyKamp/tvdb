97b9ffc08b5406fe53ca9e1bbbb057edd3c4f002
AyyKamp <32194363+AyyKamp@users.noreply.github.com>
2018-03-06 11:13:22 +0100

    Update fcm.js

--- POST/firebase/fcm.js
+++ POST/firebase/fcm.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const admin = require('./.functions/admin.js');
-const messageFunctions = require('./.functions/messageFunctions.js');
+const admin = require('./.functions/admin.js'); //Importieren des admin Moduls. Siehe .functions/admin.js
+const messageFunctions = require('./.functions/messageFunctions.js');  //Importieren der Methoden zur Versendung von Notifications (zu dt. Benachrichtigung)
 
 router.post('/', function (req, res) ***REMOVED***
-    messageFunctions.sendMessage(req.body)
-    return res.status(200).send('Message Sent!');
+    messageFunctions.sendMessage(req.body) // Aufruf der sendMessage Methode mit dem body (das JSON Objekt welches mittels POST übermittelt wird) Attribut der Anfrage als Parameter.
+    return res.status(200).send('Message Sent!'); // Status der Anfrage wird auf 200 festgelegt. Die Nachricht "Message Sent!" wird gesendet.
 ***REMOVED***);
 
 router.get('/', function (req, res) ***REMOVED***
     res.status(200).sendFile(path.join(__dirname + '/../html/site.html'));
 ***REMOVED***);
 
-module.exports = router;
+module.exports = router; // Das router Objekt wird exportiert, damit es als Modul anderweitig verwendet werden kann.
