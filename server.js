e75fabcb198eef8affc99c6f99d242bf117fb05c
AyyKamp <32194363+AyyKamp@users.noreply.github.com>
2018-03-06 11:31:30 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-require('dotenv').config();
+require('dotenv').config(); // Mithilfe des imoprtierten Moduls "dotenv" werden alle Variblen aus der .env Datei als Umgebungsvariablen zur Verfügung gestellt. Diese kann man mithilfe von process.env abrufen.
 const request = require('request');
 const ***REMOVED*** execSync ***REMOVED*** = require('child_process');
 const app = require('./app');
-let port = process.env.PORT || 4000;
+let port = process.env.PORT || 4000; // Die Variable port wird entweder auf den Wert der Umgebungsvariable PORT oder, falls diese nicht definiert ist, auf 4000 gesetzt.
 
 var server = app.listen(port, function() ***REMOVED***
-  console.log(`its working btw ($***REMOVED***port***REMOVED***) `);
+  console.log(`its working btw ($***REMOVED***port***REMOVED***) `); // Der aktuelle Port wird in die Konsole geloggt.
 ***REMOVED***);
-let headers = ***REMOVED***
-  Accept: 'application/vnd.heroku+json; version=3',
-  Authorization: 'Bearer ' + process.env.HEROKU_KEY
+let headers = ***REMOVED*** //
+  Accept: 'application/vnd.heroku+json; version=3', // Accept header für die folgende HTTP Anfrage werden festgelegt.
+  Authorization: 'Bearer ' + process.env.HEROKU_KEY // Das Attribut Authorization wird auf den String "Bearer " plus den Wert der Umgebungsvariable HEROKU_KEY gesetzt.
 ***REMOVED***;
 let options = ***REMOVED***
-  url: 'https://api.heroku.com/apps/tvdb-rest/config-vars',
+  url: 'https://api.heroku.com/apps/tvdb-rest/config-vars', 
   headers: headers
 ***REMOVED***;
 function callback(error, response, body) ***REMOVED***
