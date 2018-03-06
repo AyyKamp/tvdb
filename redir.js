5bd70e15561c7509e3a4fb69044d02d51c0a45cb
AyyKamp <32194363+AyyKamp@users.noreply.github.com>
2018-03-06 11:15:11 +0100

    Update redir.js

--- redir.js
+++ redir.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function (req, res) ***REMOVED***
-    return res.redirect(301, 'http://www.tvseriesapp.tk/#/');
+    return res.redirect(301, 'http://www.tvseriesapp.tk/#/'); //Die Anfrage wird mit dem Status code 301 auf http://www.tvseriesapp.tk/#/ weitergeleitet
     
 ***REMOVED***);
 
